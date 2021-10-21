e03089c41496acf100fe1afc1393ec3590293df5
Bilalp19 <p016979l@student.staffs.ac.uk>
2021-10-21 10:08:09 +0100

    New Source

--- Program2_dataTypeSize/Project5_IncrementDecrment.cpp
+++ Program2_dataTypeSize/Project5_IncrementDecrment.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-using namespace std; 
-
-
-int main() 
-{
-	int num = 1; 
-	cout << "num =" << num << endl; 
-	cout << "Increment Operator before num = " << ++num << endl;
-	cout << "numb after = " << num << endl; 
-
-	num = 1; 
-	
-	cout << "num = " << num << endl;
-	cout << "Increment Operator after num = " << num++ << endl;
-	cout << "num after = " << num << endl;
-
-	num = 1; 
-	
-	cout << "num =" << num << endl;
-	cout << "Decremenet Operator after num = " << --num << endl;
-	cout << "numb after = " << num << endl;
-
-	num = 1; 
-
-	cout << "num =" << num << endl;
-	cout << "Decremenet Operator after num = " << num-- << endl;
-	cout << "numb after = " << num << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std; 
+//
+//
+//int main() 
+//{
+//	int num = 1; 
+//	cout << "num =" << num << endl; 
+//	cout << "Increment Operator before num = " << ++num << endl;
+//	cout << "numb after = " << num << endl; 
+//
+//	num = 1; 
+//	
+//	cout << "num = " << num << endl;
+//	cout << "Increment Operator after num = " << num++ << endl;
+//	cout << "num after = " << num << endl;
+//
+//	num = 1; 
+//	
+//	cout << "num =" << num << endl;
+//	cout << "Decremenet Operator after num = " << --num << endl;
+//	cout << "numb after = " << num << endl;
+//
+//	num = 1; 
+//
+//	cout << "num =" << num << endl;
+//	cout << "Decremenet Operator after num = " << num-- << endl;
+//	cout << "numb after = " << num << endl;
+//
+//	return 0; 
+//}
