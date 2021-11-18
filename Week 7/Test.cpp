e6289edcca5ca173dbbd8c6265ec79e2e119a02e
Bilalp19 <p016979l@student.staffs.ac.uk>
2021-11-18 10:49:21 +0000

    Added Week 7

---
+++ Week 7/Test.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    char array[100];
+//
+//    cin >> array;
+//
+//    for (int i = 0; i < 100; i++) {
+//        if (array[i] == ' ') {
+//            cout << "space" << endl;
+//        }
+//
+//        if (array[i] != ' ') {
+//            cout << "no space" << endl;
+//        }
+//    }
+//
+//    return 0;
+//}
